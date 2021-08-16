83e23e0f141473296633593a216ce8733c7223d3
InterceptRequest <87236806+InterceptRequest@users.noreply.github.com>
2021-08-16 09:23:21 +0800

    Delete src/main directory

--- src/main/java/com/bjpowernode/crm/service/BaseService.java
+++
@@ -1,8 +0,0 @@
-package com.bjpowernode.crm.service;
-
-public interface BaseService {
-    interface TABLE {
-        String ATS = "tbl_marketing_activities";
-        String US = "tbl_user";
-    }
-}
